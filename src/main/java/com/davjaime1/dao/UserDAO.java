ca1ef37dc0afbf48b8066fc22da537d67c4f01d6
Jaime, David <davjaime1@yahoo.com>
2020-09-20 20:29:59 -0500

    Added Register Prepared Statement
    Modified the register user DAO, to now use prepared statements

--- src/main/java/com/davjaime1/dao/UserDAO.java
+++ src/main/java/com/davjaime1/dao/UserDAO.java
@@ -1,6 +1,7 @@
 package com.davjaime1.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -9,19 +10,29 @@ import com.davjaime1.model.User;
 import com.davjaime1.sql.SQLConnection;
 
 public abstract class UserDAO
-{
+{	
 	private static SQLConnection DBMgr = SQLConnection.getInstance();
 	
 	public static void addNewUser(User user)
 	{
-		Statement stmt = null;
-		Connection conn = SQLConnection.getDBConnection(); 
-		String queryString = "INSERT INTO users (username, password, email, role_id) " +
-							 "VALUES ('" + user.getUsername() +"','" + user.getPassword() + "', '" + user.getEmail() + "', '" + user.getRoleId() +"');";
 		try {
-            stmt = conn.createStatement();
-            stmt.executeUpdate(queryString);
+			Connection conn = SQLConnection.getDBConnection(); 
+			String query = "INSERT INTO users (username, password, email, role_id)" +
+								 "VALUES (?,?,?,?)";
+			//"VALUES ('" + user.getUsername() +"','" + user.getPassword() + "', '" + user.getEmail() + "', '" + user.getRoleId() +"');";
+		
+			PreparedStatement stmt = conn.prepareStatement(query);
+            stmt.setString(1, user.getUsername());
+            stmt.setString(2, user.getPassword());
+            stmt.setString(3, user.getEmail());
+            stmt.setInt(4, user.getRoleId());
+            
+            int status = stmt.executeUpdate();
+            if(status > 0) {
+               System.out.println("Record is inserted successfully !!!");
+            }
             conn.commit();
+            
 		} 
 		catch (SQLException e)
 		{
